25f3f4299a641d1c3fcd5ab1645649fe0c1dad1c
Tim Heighes <tim@heighes.com>
2019-05-11 19:40:50 +0200

    Add test for invalid url

--- test/pdflayer_test.rb
+++ test/pdflayer_test.rb
@@ -21,11 +21,27 @@ class PdflayerTest < Minitest::Test
     client = Pdflayer::Client.new(ENV['ACCESS_KEY'], ENV['SECRET_KEYWORD'])
     document_url = 'https://en.wikipedia.org/wiki/Special:Random'
     options = Pdflayer::ConvertOptions.new
+    options.test = true
     options.export = File.join('tmp', SecureRandom.uuid + '.pdf')
 
     response = client.convert(document_url, options)
-    refute_nil response
+
+    assert response[:success]
     assert File.exist?(options.export)
+
     File.unlink options.export
+  end
+
+  def test_invalid_url
+    client = Pdflayer::Client.new(ENV['ACCESS_KEY'], ENV['SECRET_KEYWORD'])
+    document_url = 'not a url'
+    options = Pdflayer::ConvertOptions.new
+    options.test = true
+    options.export = File.join('tmp', SecureRandom.uuid + '.pdf')
+
+    response = client.convert(document_url, options)
+
+    refute response[:success]
+    refute File.exist?(options.export)
   end
 end
